735c4f84edba50c9428b8b1276a4b96932d6aa90
JosephPallipadan <pallipadanjoseph@gmail.com>
2020-06-05 11:56:19 +0400

    Setup code for bot; Bot was added to test server and can listen for and read messages

---
+++ index.js
@@ -0,0 +1,23 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// Module to read environment variables easily
+const dotenv = require('dotenv');
+dotenv.config();
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+});
+
+// login to Discord with your app's token
+client.login(`${process.env.TOKEN}`);
+
