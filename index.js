80bbb0add30f048bf4d8c0236dc27c2ff4b2dcd8
JosephPallipadan <pallipadanjoseph@gmail.com>
2020-06-08 15:55:48 +0400

    Removed Commented prior code

--- index.js
+++ index.js
@@ -53,28 +53,6 @@ client.on("message", async (message) => {
         );
       }
   }
-  //   if (message.content === `${prefix}code`) {
-  //     message.reply(`Your access code is ${message.guild.id}`);
-  //   } else if (message.content === `${prefix}reset`) {
-  //     FirebaseHelpers.resetDB();
-  //   } else if (message.content === `${prefix}entries`) {
-  //     const response = await FirebaseHelpers.numEntries(
-  //       message.guild.name,
-  //       message.content.substr(prefix.length + 7) // 7 represents length of string "entries"
-  //     );
-  //     message.reply(JSON.stringify(response));
-  //   } else if (!message.author.bot) {
-  //     const exists = await FirebaseHelpers.serverExists(message.guild.id);
-  //     if (exists) {
-  //       FirebaseHelpers.addMessage(message.guild.name, message.member.user.tag);
-  //     } else {
-  //       FirebaseHelpers.addServer(
-  //         message.guild.id,
-  //         message.guild.name,
-  //         message.member.user.tag
-  //       );
-  //     }
-  //   }
 });
 
 // login to Discord with your app's token
