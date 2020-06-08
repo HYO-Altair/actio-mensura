fd4a020631b2a8428dd506d62785f103d0e023f0
JosephPallipadan <pallipadanjoseph@gmail.com>
2020-06-08 15:55:03 +0400

    Changed if conditions to switch case and placed exit condition at start according to suggestion

--- index.js
+++ index.js
@@ -19,28 +19,62 @@ client.once("ready", () => {
 });
 
 client.on("message", async (message) => {
-  if (message.content === `${prefix}code`) {
-    message.reply(`Your access code is ${message.guild.id}`);
-  } else if (message.content === `${prefix}reset`) {
-    FirebaseHelpers.resetDB();
-  } else if (message.content.startsWith(`${prefix}entries`)) {
-    const response = await FirebaseHelpers.numEntries(
-      message.guild.name,
-      message.content.substr(prefix.length + 7) // 7 represents length of string "entries"
-    );
-    message.reply(JSON.stringify(response));
-  } else if (!message.author.bot) {
-    const exists = await FirebaseHelpers.serverExists(message.guild.id);
-    if (exists) {
-      FirebaseHelpers.addMessage(message.guild.name, message.member.user.tag);
-    } else {
-      FirebaseHelpers.addServer(
-        message.guild.id,
-        message.guild.name,
-        message.member.user.tag
-      );
-    }
+  // Do nothing if it is a bot message
+  if (message.author.bot) {
+    return;
   }
+
+  switch (message.content) {
+    case `${prefix}code`:
+      message.reply(`Your access code is ${message.guild.id}`);
+      break;
+
+    case `${prefix}reset`:
+      FirebaseHelpers.resetDB();
+      break;
+
+    case `${prefix}entries`:
+      const response = await FirebaseHelpers.numEntries(
+        message.guild.name,
+        message.content.substr(prefix.length + 7) // 7 represents length of string "entries"
+      );
+      message.reply(JSON.stringify(response));
+      break;
+
+    default:
+      const exists = await FirebaseHelpers.serverExists(message.guild.id);
+      if (exists) {
+        FirebaseHelpers.addMessage(message.guild.name, message.member.user.tag);
+      } else {
+        FirebaseHelpers.addServer(
+          message.guild.id,
+          message.guild.name,
+          message.member.user.tag
+        );
+      }
+  }
+  //   if (message.content === `${prefix}code`) {
+  //     message.reply(`Your access code is ${message.guild.id}`);
+  //   } else if (message.content === `${prefix}reset`) {
+  //     FirebaseHelpers.resetDB();
+  //   } else if (message.content === `${prefix}entries`) {
+  //     const response = await FirebaseHelpers.numEntries(
+  //       message.guild.name,
+  //       message.content.substr(prefix.length + 7) // 7 represents length of string "entries"
+  //     );
+  //     message.reply(JSON.stringify(response));
+  //   } else if (!message.author.bot) {
+  //     const exists = await FirebaseHelpers.serverExists(message.guild.id);
+  //     if (exists) {
+  //       FirebaseHelpers.addMessage(message.guild.name, message.member.user.tag);
+  //     } else {
+  //       FirebaseHelpers.addServer(
+  //         message.guild.id,
+  //         message.guild.name,
+  //         message.member.user.tag
+  //       );
+  //     }
+  //   }
 });
 
 // login to Discord with your app's token
