080e83fa311de45b6786c97503c59ae7aa99ed80
Nick Chen <nickchen@usc.edu>
2020-06-16 13:43:26 -0500

    replacing spaces

--- firebasehelpers.js
+++ firebasehelpers.js
@@ -17,7 +17,9 @@ dotenv.config();
 // Initialize Firebase
 const serviceAccountKey = process.env.PATH_TO_FIREBASE_KEY;
 const Firebase = FirebaseAdmin.initializeApp({
-  credential: FirebaseAdmin.credential.cert(JSON.parse(serviceAccountKey)), // references process.env.GOOGLE_APPLICATION_CREDENTIALS
+  credential: FirebaseAdmin.credential.cert(
+    JSON.parse(serviceAccountKey.replace(/\\n/g, "\n"))
+  ), // references process.env.GOOGLE_APPLICATION_CREDENTIALS
   databaseURL: "https://actio-mensura.firebaseio.com",
 });
 
